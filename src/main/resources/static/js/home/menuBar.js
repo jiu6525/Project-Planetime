e7f494574112a9f6ca5a0b32cbe4f979c0bbefe8
jiu6525 <jiu6525@daum.net>
2023-08-02 12:24:07 +0900

    v1

---
+++ src/main/resources/static/js/home/menuBar.js
@@ -0,0 +1,17 @@
+  const divLst = document.getElementsByTagName('div')
+  console.log(divLst)
+  // 버튼가져오기
+  const bentoBtn = document.getElementById('bentoBtn');
+  bentoBtn.addEventListener('click', function() {
+    // menu 속성 바꾸기
+    const menu = document.getElementById('menu-list')
+    if (menu.style.height) {
+      menu.style.height = null;
+    } else {
+      menu.style.height = '300px';
+    }
+  })
+  // menu안에 들어갈 item을 만들어서 menu에 추가해주기
+  // for (let i = 0; i < 7; i++) {
+  //   const item = document.createElement('div')
+  // }
